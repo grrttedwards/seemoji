694e6331ac006d054fc8ca34a92110e8e2a8d63c
Garrett Edwards <garrettedwards95@gmail.com>
2017-04-07 16:46:06 -0400

    👌👌👌👌👌👌

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-function handleKeypress(e){
-    e = e || window.event;
-    // do nothing for now
+function handleKeypress(){
+    e = window.event;
+    if (e.which == 13 || e.keycode == 13) {
+      if (document.getElementById("search").value === "") {
+        document.getElementById("mojos").innerHTML = "";
+      } else {
+        var content = document.createElement("IMG");
+        content.src = "mojos/apple-okay.png";
+        content.style.width = "100%";
+        var li = document.createElement("LI");
+        li.appendChild(content);
+        var ul = document.getElementById("mojos").appendChild(li);
+        return false;
+      }
+    }
+    return true;
 }
